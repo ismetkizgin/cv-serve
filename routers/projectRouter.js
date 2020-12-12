031aef7fdc666ba573a7d8277c58782cd09f0839
ismet kizgin <info@ismetkizgin.com>
2020-12-13 01:24:58 +0300

    feat: added project find router

--- routers/projectRouter.js
+++ routers/projectRouter.js
@@ -6,40 +6,84 @@ const tokenControl = verifyToken.tokenControl;
 const projectValidator = validators.projectValidator;
 const HttpStatusCode = require("http-status-codes");
 
-router.get('/project', tokenControl, projectValidator.limitAndOffset, async (req, res) => {
+router.get(
+  "/project",
+  tokenControl,
+  projectValidator.limitAndOffset,
+  async (req, res) => {
     try {
-        const result = await projectTransactions.listAsync(req.query);
-        res.json(result);
+      const result = await projectTransactions.listAsync(req.query);
+      res.json(result);
     } catch (error) {
-        res.status(error.status || HttpStatusCode.INTERNAL_SERVER_ERROR).send(error.message);
+      res
+        .status(error.status || HttpStatusCode.INTERNAL_SERVER_ERROR)
+        .send(error.message);
     }
-});
+  }
+);
 
-router.post('/project', tokenControl, projectValidator.insert, async (req, res) => {
+router.get(
+  "/project/:Id",
+  tokenControl,
+  projectValidator.paramId,
+  async (req, res) => {
     try {
-        const result = await projectTransactions.insertAsync(req.body);
-        res.json(result);
+      const result = await projectTransactions.listAsync(req.params);
+      res.json(result[0]);
     } catch (error) {
-        res.status(error.status || HttpStatusCode.INTERNAL_SERVER_ERROR).send(error.message);
+      res
+        .status(error.status || HttpStatusCode.INTERNAL_SERVER_ERROR)
+        .send(error.message);
     }
-});
+  }
+);
 
-router.put('/project', tokenControl, projectValidator.update, async (req, res) => {
+router.post(
+  "/project",
+  tokenControl,
+  projectValidator.insert,
+  async (req, res) => {
     try {
-        const result = await projectTransactions.updateAsync(req.body);
-        res.json(result);
+      const result = await projectTransactions.insertAsync(req.body);
+      res.json(result);
     } catch (error) {
-        res.status(error.status || HttpStatusCode.INTERNAL_SERVER_ERROR).send(error.message);
+      res
+        .status(error.status || HttpStatusCode.INTERNAL_SERVER_ERROR)
+        .send(error.message);
     }
-});
+  }
+);
 
-router.delete('/project', tokenControl, projectValidator.bodyId, async (req, res) => {
+router.put(
+  "/project",
+  tokenControl,
+  projectValidator.update,
+  async (req, res) => {
     try {
-        const result = await projectTransactions.deleteAsync(req.body);
-        res.json(result);
+      const result = await projectTransactions.updateAsync(req.body);
+      res.json(result);
     } catch (error) {
-        res.status(error.status || HttpStatusCode.INTERNAL_SERVER_ERROR).send(error.message);
+      res
+        .status(error.status || HttpStatusCode.INTERNAL_SERVER_ERROR)
+        .send(error.message);
     }
-});
+  }
+);
+
+router.delete(
+  "/project",
+  tokenControl,
+  projectValidator.bodyId,
+  async (req, res) => {
+    try {
+      const result = await projectTransactions.deleteAsync(req.body);
+      res.json(result);
+    } catch (error) {
+      res
+        .status(error.status || HttpStatusCode.INTERNAL_SERVER_ERROR)
+        .send(error.message);
+    }
+  }
+);
 
 module.exports = router;
