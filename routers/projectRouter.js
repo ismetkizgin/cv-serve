eeb5c8643d698484fe53cbc82c657b29119a8776
Kadir Can KARADEMİR <50851192+CanKadirKarademir@users.noreply.github.com>
2020-12-12 22:28:00 +0300

    feat: added project list transaction

---
+++ routers/projectRouter.js
@@ -0,0 +1,23 @@
+const router = require("express")();
+const jwt = require("jsonwebtoken");
+const TransactionsFactory = require("../database/transactionFactory");
+const { validators, verifyToken } = require("../middleware");
+const projectTransactions = TransactionsFactory.creating("projectTransactions");
+const tokenControl = verifyToken.tokenControl;
+const projectValidator = validators.projectValidator;
+const HttpStatusCode = require("http-status-codes");
+
+router.get(
+    '/project',
+    tokenControl,
+    projectValidator.limitAndOffset,
+    async (req, res) => {
+        try {
+            const result = await projectTransactions.listAsync(req.query);
+            res.json(result);
+        } catch (error) {
+            res.status(error.status || HttpStatusCode.INTERNAL_SERVER_ERROR).send(error.message);
+        }
+    });
+
+module.exports = router;
