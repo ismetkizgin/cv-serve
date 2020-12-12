efa72e79702f65412003f894e45b3408096023ba
Kadir Can KARADEMİR <50851192+CanKadirKarademir@users.noreply.github.com>
2020-12-13 01:39:35 +0300

    feat: added work experience find by id transaction

--- routers/workExperienceRouter.js
+++ routers/workExperienceRouter.js
@@ -1,52 +1,97 @@
-const router = require("express")();
-const TransactionsFactory = require("../database/transactionFactory");
-const { validators, verifyToken } = require("../middleware");
-const workExperienceTransactions = TransactionsFactory.creating("workExperienceTransactions");
+const router = require('express')();
+const TransactionsFactory = require('../database/transactionFactory');
+const { validators, verifyToken } = require('../middleware');
+const workExperienceTransactions = TransactionsFactory.creating(
+  'workExperienceTransactions'
+);
 const tokenControl = verifyToken.tokenControl;
 const workExperienceValidator = validators.workExperienceValidator;
-const HttpStatusCode = require("http-status-codes");
-const { date } = require("joi");
+const HttpStatusCode = require('http-status-codes');
 
-router.get('/work-experience', tokenControl, workExperienceValidator.limitAndOffset, async (req, res) => {
+router.get(
+  '/work-experience',
+  tokenControl,
+  workExperienceValidator.limitAndOffset,
+  async (req, res) => {
     try {
-        const result = await workExperienceTransactions.listAsync(req.query);
-        res.json(result);
+      const result = await workExperienceTransactions.listAsync(req.query);
+      res.json(result);
     } catch (error) {
-        res.status(error.status || HttpStatusCode.INTERNAL_SERVER_ERROR).send(error.message);
+      res
+        .status(error.status || HttpStatusCode.INTERNAL_SERVER_ERROR)
+        .send(error.message);
     }
-});
+  }
+);
 
-router.post('/work-experience', tokenControl, workExperienceValidator.insert, async (req, res) => {
+router.get(
+  '/work-experience/:Id',
+  tokenControl,
+  workExperienceValidator.paramId,
+  async (req, res) => {
     try {
-        req.body.StartDateOfWork = new Date(req.body.StartDateOfWork);
-        if (req.body.DismissalDate)
-            req.body.DismissalDate = new Date(req.body.DismissalDate);
-        const result = await workExperienceTransactions.insertAsync(req.body);
-        res.json(result);
+      const result = await workExperienceTransactions.listAsync(req.params);
+      res.json(result[0]);
     } catch (error) {
-        res.status(error.status || HttpStatusCode.INTERNAL_SERVER_ERROR).send(error.message);
+      res
+        .status(error.status || HttpStatusCode.INTERNAL_SERVER_ERROR)
+        .send(error.message);
     }
-});
+  }
+);
 
-router.put('/work-experience', tokenControl, workExperienceValidator.update, async (req, res) => {
+router.post(
+  '/work-experience',
+  tokenControl,
+  workExperienceValidator.insert,
+  async (req, res) => {
     try {
-        req.body.StartDateOfWork = new Date(req.body.StartDateOfWork);
-        if (req.body.DismissalDate)
-            req.body.DismissalDate = new Date(req.body.DismissalDate);
-        const result = await workExperienceTransactions.updateAsync(req.body);
-        res.json(result);
+      req.body.StartDateOfWork = new Date(req.body.StartDateOfWork);
+      if (req.body.DismissalDate)
+        req.body.DismissalDate = new Date(req.body.DismissalDate);
+      const result = await workExperienceTransactions.insertAsync(req.body);
+      res.json(result);
     } catch (error) {
-        res.status(error.status || HttpStatusCode.INTERNAL_SERVER_ERROR).send(error.message);
+      res
+        .status(error.status || HttpStatusCode.INTERNAL_SERVER_ERROR)
+        .send(error.message);
     }
-});
+  }
+);
 
-router.delete('/work-experience', tokenControl, workExperienceValidator.bodyId, async (req, res) => {
+router.put(
+  '/work-experience',
+  tokenControl,
+  workExperienceValidator.update,
+  async (req, res) => {
     try {
-        const result = await workExperienceTransactions.deleteAsync(req.body);
-        res.json(result);
+      req.body.StartDateOfWork = new Date(req.body.StartDateOfWork);
+      if (req.body.DismissalDate)
+        req.body.DismissalDate = new Date(req.body.DismissalDate);
+      const result = await workExperienceTransactions.updateAsync(req.body);
+      res.json(result);
     } catch (error) {
-        res.status(error.status || HttpStatusCode.INTERNAL_SERVER_ERROR).send(error.message);
+      res
+        .status(error.status || HttpStatusCode.INTERNAL_SERVER_ERROR)
+        .send(error.message);
     }
-});
+  }
+);
+
+router.delete(
+  '/work-experience',
+  tokenControl,
+  workExperienceValidator.bodyId,
+  async (req, res) => {
+    try {
+      const result = await workExperienceTransactions.deleteAsync(req.body);
+      res.json(result);
+    } catch (error) {
+      res
+        .status(error.status || HttpStatusCode.INTERNAL_SERVER_ERROR)
+        .send(error.message);
+    }
+  }
+);
 
 module.exports = router;
