ca2a4d1e532184a92529b02dd3a8317ed25de862
ismet kizgin <info@ismetkizgin.com>
2020-12-13 01:23:08 +0300

    feat: added personnel information update router

--- routers/personalInformationRouter.js
+++ routers/personalInformationRouter.js
@@ -5,7 +5,7 @@ const personalInformationTransactions = TransactionsFactory.creating(
   "personalInformationTransactions"
 );
 const tokenControl = verifyToken.tokenControl;
-const projectValidator = validators.personalInformationValidator;
+const personalInformationValidator = validators.personalInformationValidator;
 const HttpStatusCode = require("http-status-codes");
 
 router.get("/personal-information", async (req, res) => {
@@ -18,5 +18,24 @@ router.get("/personal-information", async (req, res) => {
       .send(error.message);
   }
 });
+
+router.put(
+  "/personal-information",
+  tokenControl,
+  personalInformationValidator.update,
+  async (req, res) => {
+    try {
+      req.body.DateOfBirth = new Date(req.body.DateOfBirth);
+      const result = await personalInformationTransactions.updateAsync(
+        req.body
+      );
+      res.json(result);
+    } catch (error) {
+      res
+        .status(error.status || HttpStatusCode.INTERNAL_SERVER_ERROR)
+        .send(error.message);
+    }
+  }
+);
 
 module.exports = router;
