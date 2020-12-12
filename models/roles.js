c9bb283523796097aa7b5c495cfce061bbd822b8
ismet kizgin <info@ismetkizgin.com>
2020-12-12 21:39:08 +0300

    refactor: infrastructure was revised

--- models/roles.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    Root: 'Root',
-    Administrator: 'Administrator',
-    Developer: 'Developer',
-    Editor: 'Editor',
-    User: 'User'
-};
--- utils/routerAuthorization.js
+++
@@ -1,4 +0,0 @@
-const Roles = require('../models/roles');
-
-module.exports = {
-}
