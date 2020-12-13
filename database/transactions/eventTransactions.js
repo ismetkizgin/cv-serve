f85d118cac90d55e0baecf6b8ee25c8e866b4025
ismet kizgin <info@ismetkizgin.com>
2020-12-13 14:11:46 +0300

    feat: added event database transactions

---
+++ database/transactions/eventTransactions.js
@@ -0,0 +1,109 @@
+const { mysqlDataContext } = require('../dataContexts');
+const HttpStatusCode = require('http-status-codes');
+const { sqlHelper } = require('../../utils');
+
+class EventTransactions {
+  constructor() {
+    this._datacontext = mysqlDataContext.connection();
+  }
+
+  listAsync(values) {
+    return new Promise((resolve, reject) => {
+      this._datacontext.query(
+        `SELECT * FROM tblEvent ${sqlHelper.getWhere(
+          values
+        )} ORDER BY id ASC ${sqlHelper.getLimitOffset(values)}`,
+        (error, result) => {
+          if (!error) {
+            if (result.length > 0) resolve(result);
+            else
+              reject({
+                status: HttpStatusCode.NOT_FOUND,
+                message: 'No event registered to the system was found.'
+              });
+          } else {
+            reject({
+              status: HttpStatusCode.INTERNAL_SERVER_ERROR,
+              message: error.message
+            });
+          }
+        }
+      );
+    });
+  }
+
+  insertAsync(values) {
+    return new Promise((resolve, reject) => {
+      this._datacontext.query(
+        `INSERT INTO tblEvent SET ?`,
+        values,
+        (error, result) => {
+          if (!error) {
+            if (result.affectedRows)
+              resolve('Event registration has taken place.');
+            else
+              reject({
+                status: HttpStatusCode.INTERNAL_SERVER_ERROR,
+                message: 'Error while registering event!'
+              });
+          } else {
+            reject({
+              status: HttpStatusCode.INTERNAL_SERVER_ERROR,
+              message: error.message
+            });
+          }
+        }
+      );
+    });
+  }
+
+  updateAsync(values) {
+    return new Promise((resolve, reject) => {
+      this._datacontext.query(
+        `UPDATE tblEvent SET ? WHERE Id=?`,
+        [values, values.Id],
+        (error, result) => {
+          if (!error) {
+            if (result.affectedRows)
+              resolve('Event information has been updated.');
+            else
+              reject({
+                status: HttpStatusCode.INTERNAL_SERVER_ERROR,
+                message: 'An error occurred while updating event information.'
+              });
+          } else {
+            reject({
+              status: HttpStatusCode.INTERNAL_SERVER_ERROR,
+              message: error.message
+            });
+          }
+        }
+      );
+    });
+  }
+
+  deleteAsync(values) {
+    return new Promise((resolve, reject) => {
+      this._datacontext.query(
+        `DELETE FROM tblEvent ${sqlHelper.getWhere(values)}`,
+        (error, result) => {
+          if (!error) {
+            if (result.affectedRows) resolve('Deletion succeeded.');
+            else
+              reject({
+                status: HttpStatusCode.GONE,
+                message: 'There is no such event.'
+              });
+          } else {
+            reject({
+              status: HttpStatusCode.INTERNAL_SERVER_ERROR,
+              message: error.message
+            });
+          }
+        }
+      );
+    });
+  }
+}
+
+module.exports = EventTransactions;
