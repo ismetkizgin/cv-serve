1f1d2d37c8ef1e961bf164464ccb448ff3eb22a7
ismet kizgin <info@ismetkizgin.com>
2020-12-13 14:23:39 +0300

    feat: added activities attended validator

---
+++ middleware/validators/activitiesAttendedValidator.js
@@ -0,0 +1,40 @@
+const joi = require('joi');
+const HttpStatusCode = require('http-status-codes');
+const CommonValidator = require('./commonValidator');
+
+class ActivitiesAttendedValidator extends CommonValidator {
+  constructor() {}
+
+  static async insert(req, res, next) {
+    try {
+      await joi
+        .object({
+          Description: joi.string().required()
+        })
+        .validateAsync(req.body);
+      next();
+    } catch (error) {
+      res
+        .status(HttpStatusCode.EXPECTATION_FAILED)
+        .send('Must have correct data entry.');
+    }
+  }
+
+  static async update(req, res, next) {
+    try {
+      await joi
+        .object({
+          Id: joi.number().required(),
+          Description: joi.string().required()
+        })
+        .validateAsync(req.body);
+      next();
+    } catch (error) {
+      res
+        .status(HttpStatusCode.EXPECTATION_FAILED)
+        .send('Must have correct data entry.');
+    }
+  }
+}
+
+module.exports = ActivitiesAttendedValidator;
